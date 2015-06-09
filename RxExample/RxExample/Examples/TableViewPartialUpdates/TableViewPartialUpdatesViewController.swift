3f583857957fed535b581dbb034a8f378d0c70f0
Krunoslav Zaher <kzaher@Krunoslavs-MacBook-Pro.local>
2015-06-09 06:45:54 +0200

    WIP

---
+++ RxExample/RxExample/Examples/TableViewPartialUpdates/TableViewPartialUpdatesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewPartialUpdatesViewController.swift
+//  RxExample
+//
+//  Created by Krunoslav Zaher on 6/8/15.
+//  Copyright (c) 2015 Krunoslav Zaher. All rights reserved.
+//
+
+import Foundation
+
+class TableViewPartialUpdates : ViewController {
+    @IBOutlet weak var reloadTableViewOutlet: UITableView!
+    @IBOutlet weak var partialUpdatesTableViewOutlet: UITableView!
+}
